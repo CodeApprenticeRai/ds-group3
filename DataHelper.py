264a5da82ea34661b66007b95bc25cf0df3631d1
mhd0528 <1400012862@pku.edu.cn>
2019-11-11 15:27:52 -0500

    get data from Alpha and save in AlphaData folder

--- DataHelper.py
+++ DataHelper.py
@@ -1,5 +1,6 @@
 import json, requests, pandas as pd
 import os
+import time
 
 class DataHelper:
     def __init__(self):
@@ -11,7 +12,7 @@ class DataHelper:
         self.default_request_params = {
             # "symbol": None, # required
             "function": "TIME_SERIES_DAILY",
-            "outputsize": "compact",
+            "outputsize": "full",
             "datatype": "json",
             "apikey": self.alpha_vantage_api_key
         }
@@ -29,6 +30,7 @@ class DataHelper:
 
         return response.json()
 
+
     '''
         Returns price-history for given
         tickers as a datetime-indexed dataframe
@@ -36,13 +38,37 @@ class DataHelper:
 
     def get_data(self, list_of_ticker_symbols):
         master_df = pd.DataFrame()
+        notExist = []
+        i = 0
+        for i in range(len(list_of_ticker_symbols)):
+            ticker_symbol = list_of_ticker_symbols[i]
+            ex = 0
+            while True:
+                try:
+                    data = self._get_data(ticker_symbol)
+                    print(ticker_symbol)
+                    data_as_df = pd.DataFrame.from_dict(data["Time Series (Daily)"],
+                                                        orient='index')  # !! flagged for unoptimized
+                    #save stock data to file
+                    path = './AlphaData/' + str(ticker_symbol) + '.csv'
+                    head = ['date', 'open', 'high', 'low', 'close', 'volume']
+                    data_as_df = data_as_df.reset_index()
+                    #data_as_df.columns = head
+                    data_as_df.to_csv(path, header = head, sep=',', index=False)
+                    # extract closing price
+                    #master_df[ticker_symbol] = data_as_df["4. close"].apply( lambda price : float(price) )
+                    i += 1
+                    break
+                except Exception: # Try to catch something more specific
+                    ex += 1
+                    if ex == 2 :
+                        #record this as not exist and get data for next one
+                        i += 1
+                        ex = 0
+                        notExist.append(ticker_symbol)
+                        break
+                    else:
+                        time.sleep(60)
+                    pass
 
-        for ticker_symbol in list_of_ticker_symbols:
-            data = self._get_data(ticker_symbol)
-            data_as_df = pd.DataFrame.from_dict(data["Time Series (Daily)"],
-                                                orient='index')  # !! flagged for unoptimized
-
-            # extract closing price
-            master_df[ticker_symbol] = data_as_df["4. close"].apply( lambda price : float(price) )
-
-        return master_df
+        return notExist
--- simple-model.py
+++ simple-model.py
@@ -3,6 +3,8 @@ import pandas as pd
 import statsmodels.api as sm
 import numpy as np
 import DataHelper
+import os
+import time
 #import pyspark
 '''
 Download monthly prices of Facebook and S&P 500 index from 2014 to 2017
@@ -45,9 +47,20 @@ def individual_Beta(ind_data, stand_data):
     print(results.params[1])
     return results.params[1]
 
+
+#stock list(all file names)
+stock_list = ['ACAS', 'ANDV', 'APC', 'APOL', 'ARG', 'BCR', 'BDK', 'BEAM', 'BF.B', 'BJS', 'BMS', 'BRCM', 'BRLI', 'CAM', 'CBG', 'CCMO', 'CFC+A', 'CFN', 'COH', 'CPWR', 'CVC', 'DPS', 'DV', 'DWDP', 'EDS', 'EK', 'ERTS', 'ESV', 'FDO', 'FMCC', 'FNMA', 'FRX', 'GENZ', 'GGP', 'HCBK', 'HCN', 'HRS', 'HSP', 'IACI', 'JAVA', 'JDSU', 'JNS', 'JOY', 'KFT', 'KG', 'KORS', 'LEHMQ', 'LIZ', 'LLL', 'LLTC', 'LO', 'LTD', 'LUK', 'LXK', 'MHP', 'MHS', 'MI', 'MIL', 'MJN', 'MON', 'MWV', 'MWW', 'MYL', 'NAVI', 'NBL', 'NBR', 'NCC', 'NCLH', 'NDAQ', 'NE', 'NEE', 'NEM', 'NFLX', 'NFX', 'NI', 'NKE', 'NLSN', 'NOC', 'NOV', 'NOVL', 'NRG', 'NSC', 'NSM', 'NTAP', 'NTRS', 'NU', 'NUE', 'NVDA', 'NVLS', 'NWL', 'NWS', 'NWSA', 'NYT', 'NYX', 'O', 'ODP', 'OI', 'OKE', 'OMC', 'OMX', 'ORCL', 'ORLY', 'OXY', 'PAYX', 'PBCT', 'PBG', 'PBI', 'PCAR', 'PCG', 'PCL', 'PCLN', 'PCP', 'PCS', 'PDCO', 'PEG', 'PEP', 'PFE', 'PFG', 'PG', 'PGN', 'PGR', 'PH', 'PHM', 'PKG', 'PKI', 'PLD', 'PLL', 'PM', 'PNC', 'PNR', 'PNW', 'POM', 'PPG', 'PPL', 'PRGO', 'PRU', 'PSA', 'PSX', 'PTV', 'PVH', 'PWR', 'PX', 'PXD', 'PYPL', 'Q', 'QCOM', 'QEP', 'QLGC', 'QRVO', 'R', 'RAI', 'RCL', 'RDC', 'RE', 'REG', 'REGN', 'RF', 'RHI', 'RHT', 'RIG', 'RJF', 'RL', 'RMD', 'ROH', 'ROK', 'ROP', 'ROST', 'RRC', 'RRD', 'RSG', 'RSH', 'RTN', 'S', 'SAF', 'SAI', 'SBAC', 'SBUX', 'SCG', 'SCHW', 'SDS', 'SE', 'SEE', 'SGP', 'SH', 'SHLD', 'SHW', 'SIAL', 'SIG', 'SII', 'SINE_FAST', 'SINE_FAST_NOISE', 'SINE_SLOW', 'SINE_SLOW_NOISE', 'SJM', 'SLB', 'SLE', 'SLG', 'SLM', 'SNDK', 'SNI', 'SPLS', 'STJ', 'SVU', 'SWY', 'T', 'TE', 'TIE', 'TMK', 'TSO', 'TWC', 'TYC', 'VIA.B', 'WAG', 'WAMUQ', 'WFM', 'WFR', 'WFT', 'WIN', 'WLP', 'WPI', 'WPO', 'WWY', 'WYE', 'WYN', 'XTO', 'YHOO', 'ZMH']
+print(len(stock_list))
+'''
+for filename in os.listdir('./mergedQSTKandKaggleData/'):
+    #a stock file
+    if filename[-3:] == 'csv':
+        if filename[:4] != 'ML4T' and filename[0] != '$' :
+            stock_list.append(filename[:-4])
+'''
 #use DataHelper to get data
 dh = DataHelper.DataHelper()
-example_stock_list = "CHK AMD GE UBER BAC GDX BABA KR FIT GOLD AAPL M MS SIRI MSFT".split(" ")
-example_df = dh.get_data(example_stock_list[:3])
-print(example_df)
+nonExist = dh.get_data(stock_list)
+#stocks_df = dh.get_data(stock_list)
+print(nonExist)
 #individual_Beta('./mergedQSTKandKaggleData/AA.csv', './mergedQSTKandKaggleData/SPY.csv')
